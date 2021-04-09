8abe15f1bc4a70d63ec01d3ee1220aad30195da8
urban.flame <urban.fl4m3@gmail.com>
2021-04-09 20:56:47 +0300

    Added additional libs, fixed tensorflow dll, added first test

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -10,5 +10,13 @@ public class Test : MonoBehaviour
     void Start()
     {
         var a = tf.constant(2);
+        var b = tf.constant(3);
+        var c = tf.constant(5);
+
+        var mean = tf.reduce_mean(new[] {a, b, c});
+        var sum = tf.reduce_sum(new[] {a, b, c});
+        
+        Debug.Log($"Mean: {mean}");
+        Debug.Log($"Sum: {sum}");
     }
 }
