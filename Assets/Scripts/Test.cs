00a8d234fb86df088ca2d302e0054396e72b25cf
urban.flame <urban.fl4m3@gmail.com>
2021-04-10 17:53:16 +0300

    Added test for TF

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,22 +1,43 @@
-using UnityEngine;
-using Tensorflow;
-using static Tensorflow.Binding;
+using System.Collections.Generic;
 using NumSharp;
-using System.IO;
-
+using Tensorflow;
+using Tensorflow.Keras;
+using Tensorflow.Keras.ArgsDefinition;
+using Tensorflow.Keras.Layers;
+using Tensorflow.Keras.Optimizers;
+using UnityEngine;
+using static Tensorflow.Binding;
 
 public class Test : MonoBehaviour
 {
-    void Start()
+    private void Start()
     {
-        var a = tf.constant(2);
-        var b = tf.constant(3);
-        var c = tf.constant(5);
+        var array = new NDArray(new float[]
+        {
+            1, 2, 3
+        });
 
-        var mean = tf.reduce_mean(new[] {a, b, c});
-        var sum = tf.reduce_sum(new[] {a, b, c});
+        var labels = new NDArray(new float[]
+        {
+            Function(1), Function(2), Function(3)
+        });
+
+        var dataset = tf.data.Dataset.from_tensor_slices(array);
+        var dataset2 = tf.data.Dataset.from_tensor_slices(labels);
+
+        var l0 = new Dense(new DenseArgs {Units = 3});
+        var model = KerasApi.keras.Sequential(new List<ILayer> { l0 });
         
-        Debug.Log($"Mean: {mean}");
-        Debug.Log($"Sum: {sum}");
+        var losses = KerasApi.keras.losses.SparseCategoricalCrossentropy(from_logits: true);
+        var optimizer = KerasApi.keras.optimizers.SGD(1e-6f);
+        model.compile(losses, optimizer, new []{ "accuracy"});
+        model.fit(dataset, dataset2, epochs: 100);
+        
+        Debug.Log($"Layer weight {l0.weights}");
+    }
+
+    private float Function(float x)
+    {
+        return 4 * x * x + 3;
     }
 }
